437e608ba7f358f053de41c00fe3fed046ef3d18
unknown <Shaanan Cohney@ShaananHP.(none)>
2012-09-05 19:04:03 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Octoapp::Application.config.secret_token = '72d4471ec2834c0b25b0ce31b5e16a1e76f1a7df2ed952c0fa987abe35b9538dfad481630643acbb29b46664569f09e5b0d545a2ef599af0618ed123b97547ae'
