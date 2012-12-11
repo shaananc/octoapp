69dd030afa2cb8fa99f8cc50f935b6d05ca95d70
Shaanan Cohney <shaananc@gmail.com>
2012-12-11 04:30:48 -0500

    Fixed Mailer Port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Octoapp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'pennhookups.herokuapp.com:3000' }
+  config.action_mailer.default_url_options = { :host => 'pennhookups.herokuapp.com' }
 
 
 
