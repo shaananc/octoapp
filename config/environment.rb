9d02d62a7037e9c04afbb9bf08b883a9fb18fb89
Shaanan Cohney <shaananc@gmail.com>
2012-12-11 04:08:42 -0500

    Changed environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => "heroku.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
   :authentication => :plain,
-  :enable_starttls_auto => true
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com'
 }
+ActionMailer::Base.delivery_method = :smtp
 
 # Initialize the rails application
 Octoapp::Application.initialize!
